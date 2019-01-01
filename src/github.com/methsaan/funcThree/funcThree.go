119215f7204f612ad989a1f769c5a454fedcd5c4
Mohan Wickramasinghe <mwickramasinghe@nbsps.com>
2019-01-01 18:06:37 -0500

    constants - prime numbers

--- src/github.com/methsaan/funcThree/funcThree.go
+++ src/github.com/methsaan/funcThree/funcThree.go
@@ -6,27 +6,28 @@ import (
 )
 
 func isPrime(x int) bool {
+
 	var numberOfFactors,i int = 0,1
-	for i < x {
+
+	for i <= x {
 		if float64(x)/float64(i) == float64(x/i) {
 			numberOfFactors++
 		}
 		i++
 	}
-	numberOfFactors++
+
 	if numberOfFactors == 2 {
 		return true
 	}else {
 		return false
 	}
+
 }
 
 func main() {
-	var i int = 0
-
-	numbers := [5]int{2,16,5,35,2797}
-	for i < 5 {
-		fmt.Printf("%d is prime: %v\n", numbers[i], isPrime(numbers[i]))
+	var i,cnt int = 0,22
+	for i < cnt {
+		fmt.Printf("%d is prime: %v\n", i, isPrime(i))
 		i++
 	}
 }
