89a893ef30686a2267fc3da877d19805d143c31b
Methsaan <methsaan@gmail.com>
2019-02-28 18:31:16 -0500

    print and accept rune slices

--- src/arraySlice/arraySlice.go
+++ src/arraySlice/arraySlice.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 )
 
-func getstr(str *[]rune) {
-	fmt.Scanf("%s", string(*str))
+func getstr() []rune {
+	var s string
+	fmt.Scanf("%s", &s)
+	return []rune(s)
+}
+func printstr(str []rune) {
+	for x := 0; x < len(str); x++ {
+		fmt.Printf("%c", str[x])
+	}
+	fmt.Println()
 }
 func main() {
-	var str[8]rune
-	var stringSlice = str[:]
 	fmt.Print("Enter a string: ")
-	getstr(&stringSlice)
-	fmt.Println(str)
+	str := getstr()
+	printstr(str)
 }
