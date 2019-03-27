f224d6e3d4d7b7d37b15ddce65238a7536a5deba
Methsaan <methsaan@gmail.com>
2019-03-26 20:16:01 -0400

    decimal log

--- src/logarithm/logarithm.go
+++ src/logarithm/logarithm.go
@@ -14,12 +14,21 @@ func logarithm(b float64, n float64) int {
 	return p
 }
 
+func declog(b float64, n float64) float64 {
+	var p float64 = 0.000000
+	for x := 0; !(math.Abs(math.Pow(b, p)-n) < 0.001); x++ {
+		p += 0.00001
+	}
+	return p
+}
+
 func main() {
-	var num float64
 	var base float64
-	fmt.Println("Enter a number: ")
-	fmt.Scanf("%f", &num)
+	var num float64
 	fmt.Println("Enter the base: ")
 	fmt.Scanf("%f", &base)
-	fmt.Println(logarithm(base, num))
+	fmt.Println("Enter a number: ")
+	fmt.Scanf("%f", &num)
+	fmt.Println("Integer logarithm: ", logarithm(base, num))
+	fmt.Println("Accurate logarithm: ", declog(base, num))
 }
