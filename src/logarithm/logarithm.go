11be6e10c42de044b16185d0625e27f6a07dead4
Methsaan <methsaan@gmail.com>
2019-08-28 15:16:13 -0400

    check if a number is a perfect number

--- src/logarithm/logarithm.go
+++ src/logarithm/logarithm.go
@@ -5,8 +5,6 @@ import(
 	"math"
 )
 
-func isIn(float64 arr[], float64 num){
-}
 func logarithm(b float64, n float64) int {
 	var p int = 0
 	for x := 0; !(math.Abs(n-1.000000) < 0.3) && x < 32767; x++{
@@ -17,19 +15,7 @@ func logarithm(b float64, n float64) int {
 }
 
 func declog(b float64, n float64) float64 {
-	var p float64 = 0.0000000000000
-	var powers []uint = uint[1000]
-	for x := 0; x < 1000; x++ {
-		powers[x] = math.Pow(b, x)
-	}
-	for x := 0; !(math.Abs(math.Pow(b, p)-n) < 0.00001); x++ {
-		p += 0.00001
-		if (isIn(power, math.Pow(b, p))) {
-			fmt.Println("Loss of precision")
-		}
-		fmt.Printf("%.5f\n", p)
-	}
-	return p+0.00000001
+	return math.Log(n)/math.Log(b)
 }
 
 func main() {
