ffb10f787a96d5a700246bed809777d0d763854a
Methsaan <methsaan@gmail.com>
2021-02-19 21:29:10 -0500

    declare factor pair

--- src/primefactorize/primefactorize.go
+++ src/primefactorize/primefactorize.go
@@ -18,6 +18,7 @@ func factors(number int) []int {
 }
 
 func main() {
+	// powers of 2 +1
 	pow2 := [20]int{2, 3, 5, 9, 17, 33, 65, 129, 257, 513, 1025, 2049, 4097, 8193, 16385, 32769, 65537, 131073, 262525, 524289}
 	factorTree := make([][]int, 20)
 	for x := 0; x < 20; x++ {
@@ -31,19 +32,20 @@ func main() {
 		factorTree[x][0] = 0
 	}
 	factorTree[0][1] = num
-	var factoredSwitch bool = false
-	// can't run
+	var factoredSwitch bool = true
+	factorPair := make([]int, 2)
 	for x := 1; x < 5; x++ {
 		for y := 1; y < pow2[x]; y++ {
-			if factoredSwitch {
-				factorPair := [factorTree[x-1][y/2]], factorTree[x-1][(y+1)/2]]
+			if factoredSwitch { // runs every 2 iterations, find factors
+				factorPair[0] = // -----------------------------------------------------------------------------------------------------------------------------------
+				factorPair[1] = // -----------------------------------------------------------------------------------------------------------------------------------
 			}
-			if y%2 == 0 {
+			if y%2 == 0 { // second factor
 				factorTree[x][y] = factorTree[x-1][y/2]
-				factoredSwitch = !factoredSwitch
-			} else {
+			} else { // first factor
 				factorTree[x][y] = factorTree[x-1][(y+1)/2]
 			}
+			factoredSwitch = !factoredSwitch
 		}
 	}
 	fmt.Println(factorTree[0])
