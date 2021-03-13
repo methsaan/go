d9a2f51193cd4e39e216550766b8fee38929de66
Methsaan <methsaan@gmail.com>
2021-03-12 23:19:38 -0500

    slice repeat test;

--- src/primefactorize/primefactorize.go
+++ src/primefactorize/primefactorize.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"bytes"
 	"strconv"
 	"math/rand"
 )
@@ -18,7 +19,12 @@ func factors(number int) []int {
 }
 
 func main() {
-	fmt.Println(factors(100))
+	slice1 := make([]int, 3)
+	slice1[0] = 1
+	slice1[1] = 2
+	slice1[2] = 3
+	res1 := bytes.Repeat(slice1, 3)
+	fmt.Printf("Result: %s\n", res1)
 	// powers of 2 +1
 	pow2 := [20]int{2, 3, 5, 9, 17, 33, 65, 129, 257, 513, 1025, 2049, 4097, 8193, 16385, 32769, 65537, 131073, 262525, 524289}
 	factorTree := make([][]int, 20)
@@ -35,8 +41,12 @@ func main() {
 	factorTree[0][1] = num
 	var factoredSwitch bool = false
 	factorPair := make([]int, 2)
-	// loop until all prime
-	for x := 1; x < 5; x++ {
+	for x := 1; x < 6; x++ {
+		// see if previous row is equal to [0] + ([-1] * length of previous row)
+		fmt.Println(factorTree[x-1])
+		//if factorTree[x-1] {
+		//	
+		//}
 		for y := 1; y < pow2[x]; y++ {
 			if !factoredSwitch { // runs every 2 iterations, find factors
 				prevRowFactors := factors(factorTree[x-1][(y+1)/2])
@@ -61,4 +71,5 @@ func main() {
 	fmt.Println(factorTree[2])
 	fmt.Println(factorTree[3])
 	fmt.Println(factorTree[4])
+	fmt.Println(factorTree[5])
 }
